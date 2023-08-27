8e39e3944bb3e68b2f27bc3027c1dfc723726128
John Paul E. Balandan, CPA <paulbalandan@gmail.com>
2023-08-27 17:00:24 +0800

    Add use trait

--- tests/Rules/Superglobals/SuperglobalAccessRuleTest.php
+++ tests/Rules/Superglobals/SuperglobalAccessRuleTest.php
@@ -15,6 +15,7 @@ namespace CodeIgniter\PHPStan\Tests\Superglobals;
 
 use CodeIgniter\PHPStan\Rules\Superglobals\SuperglobalAccessRule;
 use CodeIgniter\PHPStan\Rules\Superglobals\SuperglobalRuleHelper;
+use CodeIgniter\PHPStan\Tests\AdditionalConfigFilesTrait;
 use PHPStan\Rules\Rule;
 use PHPStan\Testing\RuleTestCase;
 use PHPUnit\Framework\Attributes\Group;
@@ -27,6 +28,8 @@ use PHPUnit\Framework\Attributes\Group;
 #[Group('Integration')]
 final class SuperglobalAccessRuleTest extends RuleTestCase
 {
+    use AdditionalConfigFilesTrait;
+
     protected function getRule(): Rule
     {
         return new SuperglobalAccessRule(new SuperglobalRuleHelper());
