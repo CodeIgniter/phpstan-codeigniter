d06bd26241be4cc871536dbf91cf234595a9fe1b
paulbalandan <paulbalandan@users.noreply.github.com>
2023-09-11 07:08:20 +0000

    Release PHPStan CodeIgniter to target PHP 7.4

--- src/Type/ServicesReturnTypeHelper.php
+++ src/Type/ServicesReturnTypeHelper.php
@@ -45,6 +45,11 @@ final class ServicesReturnTypeHelper
     ];
 
     /**
+     * @readonly
+     */
+    private ReflectionProvider $reflectionProvider;
+
+    /**
      * @var array<int, class-string>
      */
     private array $services;
@@ -58,10 +63,11 @@ final class ServicesReturnTypeHelper
      * @param array<int, class-string> $additionalServices
      */
     public function __construct(
-        private readonly ReflectionProvider $reflectionProvider,
+        ReflectionProvider $reflectionProvider,
         array $additionalServices
     ) {
-        $this->services = [
+        $this->reflectionProvider = $reflectionProvider;
+        $this->services           = [
             FrameworkServices::class,
             AppServices::class,
             ...$additionalServices,
