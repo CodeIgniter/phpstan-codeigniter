2bb53ee272285aa63e327c75ff7f4510e5467811
John Paul E. Balandan, CPA <paulbalandan@gmail.com>
2023-09-16 20:28:15 +0800

    Extract return type inference

--- src/Type/FakeFunctionReturnTypeExtension.php
+++ src/Type/FakeFunctionReturnTypeExtension.php
@@ -15,61 +15,17 @@ namespace CodeIgniter\PHPStan\Type;
 
 use PhpParser\Node\Expr\FuncCall;
 use PHPStan\Analyser\Scope;
-use PHPStan\Reflection\ClassReflection;
 use PHPStan\Reflection\FunctionReflection;
-use PHPStan\Reflection\ReflectionProvider;
-use PHPStan\ShouldNotHappenException;
-use PHPStan\Type\BooleanType;
-use PHPStan\Type\Constant\ConstantArrayType;
-use PHPStan\Type\Constant\ConstantStringType;
 use PHPStan\Type\DynamicFunctionReturnTypeExtension;
-use PHPStan\Type\IntegerType;
 use PHPStan\Type\NonAcceptingNeverType;
-use PHPStan\Type\ObjectType;
-use PHPStan\Type\ObjectWithoutClassType;
-use PHPStan\Type\StringType;
 use PHPStan\Type\Type;
-use stdClass;
 
 final class FakeFunctionReturnTypeExtension implements DynamicFunctionReturnTypeExtension
 {
-    /**
-     * @var array<string, class-string<Type>>
-     */
-    private static array $notStringFormattedFields = [
-        'success' => BooleanType::class,
-        'user_id' => IntegerType::class,
-    ];
-
-    /**
-     * @var array<string, class-string<Type>>
-     */
-    private static array $typeInterpolations = [
-        'bool' => BooleanType::class,
-        'int'  => IntegerType::class,
-    ];
-
-    /**
-     * @var list<string>
-     */
-    private array $dateFields = [];
-
-    /**
-     * @param array<string, string> $notStringFormattedFieldsArray
-     */
     public function __construct(
+        private readonly ModelFetchedReturnTypeHelper $modelFetchedReturnTypeHelper,
         private readonly FactoriesReturnTypeHelper $factoriesReturnTypeHelper,
-        private readonly ReflectionProvider $reflectionProvider,
-        array $notStringFormattedFieldsArray
-    ) {
-        foreach ($notStringFormattedFieldsArray as $field => $type) {
-            if (! isset(self::$typeInterpolations[$type])) {
-                continue;
-            }
-
-            self::$notStringFormattedFields[$field] = self::$typeInterpolations[$type];
-        }
-    }
+    ) {}
 
     public function isFunctionSupported(FunctionReflection $functionReflection): bool
     {
@@ -98,83 +54,6 @@ final class FakeFunctionReturnTypeExtension implements DynamicFunctionReturnType
 
         $classReflection = current($classReflections);
 
-        $returnType = $this->getNativeStringPropertyValue($classReflection, $scope, 'returnType');
-
-        if ($returnType === 'object') {
-            return new ObjectType(stdClass::class);
-        }
-
-        if ($returnType === 'array') {
-            return $this->getArrayReturnType($classReflection, $scope);
-        }
-
-        if ($this->reflectionProvider->hasClass($returnType)) {
-            return new ObjectType($returnType);
-        }
-
-        return new ObjectWithoutClassType();
-    }
-
-    private function getArrayReturnType(ClassReflection $classReflection, Scope $scope): Type
-    {
-        $this->fillDateFields($classReflection, $scope);
-        $fieldsTypes = $this->getNativePropertyType($classReflection, $scope, 'allowedFields')->getConstantArrays();
-
-        if ($fieldsTypes === []) {
-            return new ConstantArrayType([], []);
-        }
-
-        $fields = array_filter(array_map(
-            static fn (Type $type) => current($type->getConstantStrings()),
-            current($fieldsTypes)->getValueTypes()
-        ));
-
-        return new ConstantArrayType(
-            $fields,
-            array_map(function (ConstantStringType $fieldType) use ($classReflection, $scope): Type {
-                $field = $fieldType->getValue();
-
-                if (array_key_exists($field, self::$notStringFormattedFields)) {
-                    $type = self::$notStringFormattedFields[$field];
-
-                    return new $type();
-                }
-
-                if (
-                    in_array($field, $this->dateFields, true)
-                    && $this->getNativeStringPropertyValue($classReflection, $scope, 'dateFormat') === 'int'
-                ) {
-                    return new IntegerType();
-                }
-
-                return new StringType();
-            }, $fields)
-        );
-    }
-
-    private function fillDateFields(ClassReflection $classReflection, Scope $scope): void
-    {
-        foreach (['createdAt', 'updatedAt', 'deletedAt'] as $property) {
-            if ($classReflection->hasNativeProperty($property)) {
-                $this->dateFields[] = $this->getNativeStringPropertyValue($classReflection, $scope, $property);
-            }
-        }
-    }
-
-    private function getNativePropertyType(ClassReflection $classReflection, Scope $scope, string $property): Type
-    {
-        if (! $classReflection->hasNativeProperty($property)) {
-            throw new ShouldNotHappenException(sprintf('Native property %s::$%s does not exist.', $classReflection->getDisplayName(), $property));
-        }
-
-        return $scope->getType($classReflection->getNativeProperty($property)->getNativeReflection()->getDefaultValueExpression());
-    }
-
-    private function getNativeStringPropertyValue(ClassReflection $classReflection, Scope $scope, string $property): string
-    {
-        $propertyType = $this->getNativePropertyType($classReflection, $scope, $property)->getConstantStrings();
-        assert(count($propertyType) === 1);
-
-        return current($propertyType)->getValue();
+        return $this->modelFetchedReturnTypeHelper->getFetchedReturnType($classReflection, $scope);
     }
 }
