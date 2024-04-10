3c2488bd84afc24d15dc2a23ff6e11be2e9e209c
John Paul E. Balandan, CPA <paulbalandan@gmail.com>
2024-04-11 00:06:55 +0800

    Add explicit callback for array_filter

--- src/Type/ModelFetchedReturnTypeHelper.php
+++ src/Type/ModelFetchedReturnTypeHelper.php
@@ -108,10 +108,13 @@ final class ModelFetchedReturnTypeHelper
             return new ConstantArrayType([], []);
         }
 
-        $fields = array_filter(array_map(
-            static fn (Type $type) => current($type->getConstantStrings()),
-            current($fieldsTypes)->getValueTypes()
-        ));
+        $fields = array_filter(
+            array_map(
+                static fn (Type $type) => current($type->getConstantStrings()),
+                current($fieldsTypes)->getValueTypes()
+            ),
+            static fn (ConstantStringType|false $constantStringType): bool => $constantStringType !== false
+        );
 
         return new ConstantArrayType(
             $fields,
